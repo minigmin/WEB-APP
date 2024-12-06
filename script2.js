553341f42222558a16cc91bce1af34991ce4bbc4
minigmin <135083983+minigmin@users.noreply.github.com>
2024-12-07 00:10:49 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
-  //사이드바 내 버튼 인터렉션//
-  const currentPage = window.location.pathname.split('/').pop(); // 파일 이름만 가져옴 (index.html)
+  $('.button').removeClass('active');
 
-  // 각 버튼의 href와 현재 페이지 비교
-  $('.button').each(function () {
-    const link = $(this).attr('href').split('/').pop(); // href의 파일 이름만 가져옴
-
-    // 현재 페이지와 href가 일치하면 active 클래스 추가
-    if (currentPage === link) {
-      $(this).addClass('active');
-    }
-  });
+  // 첫 번째 버튼에 active 클래스 추가
+  $('.button:last').addClass('active');
 });
